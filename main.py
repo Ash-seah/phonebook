ffb133736cb50c521d81b7e76ebc5975f7632766
Ashkan Asef <ash.seah.1@gmail.com>
2024-09-25 14:34:26 +0330

    results are now returned sorted

--- main.py
+++ main.py
@@ -23,18 +23,16 @@ app = FastAPI()
 async def root():
     session = SessionLocal()
     try:
-        res = session.execute(select(NameNum)).scalars().all()
+        res = session.execute(select(NameNum).order_by(NameNum.name)).scalars().all()
         return [row.__dict__ for row in res]
     finally:
         session.close()
 
-#TODO make this happen in SQL --DONE
-#TODO
 @app.put("/")
-async def add_record(name:str, number:str):
+async def add_record(name: str, number: str, email: str):
     session = SessionLocal()
     try:
-        new_record = NameNum(name=name, phone_number=number)
+        new_record = NameNum(name=name, phone_number=number, email=email)
         session.add(new_record)
         session.commit()
         return {"message": "Record added successfully"}
@@ -45,28 +43,42 @@ async def add_record(name:str, number:str):
         session.close()
 
 @app.get("/query/name/")
-async def query_name(name:str):
+async def query_name(name: str):
     session = SessionLocal()
     try:
-        res = session.query(NameNum).filter(NameNum.name.ilike(f"%{name}%")).all()
+        res = session.query(NameNum).filter(NameNum.name.ilike(f"%{name}%")).order_by(NameNum.name).all()
         return [row.__dict__ for row in res]
     finally:
         session.close()
 
 @app.get("/query/number/")
-async def query_number(number:str):
+async def query_number(number: str):
     session = SessionLocal()
     try:
-        res = session.query(NameNum).filter(NameNum.phone_number.like(f"%{number}%")).all()
+        res = session.query(NameNum).filter(NameNum.phone_number.like(f"%{number}%")).order_by(NameNum.name).all()
         return [row.__dict__ for row in res]
     finally:
         session.close()
 
 @app.get("/query/email/")
-async def query_number(email:str):
+async def query_email(email: str):
     session = SessionLocal()
     try:
-        res = session.query(NameNum).filter(NameNum.email.like(f"%{email}%")).all()
+        res = session.query(NameNum).filter(NameNum.email.like(f"%{email}%")).order_by(NameNum.name).all()
         return [row.__dict__ for row in res]
+    finally:
+        session.close()
+
+@app.delete("/delete/")
+async def delete_record(name: str):
+    session = SessionLocal()
+    try:
+        record_to_delete = session.query(NameNum).filter(NameNum.name == name).first()
+        if record_to_delete:
+            session.delete(record_to_delete)
+            session.commit()
+            return {"message": "Record deleted successfully"}
+        else:
+            raise HTTPException(status_code=404, detail="Record not found")
     finally:
         session.close()
